5d822a2b4c712f8dd8fdae8416781529aefd2b5e
Rathana Voy <rathanavoy@RathanaMacbookPro.local>
2019-06-16 11:23:50 +0700

    init

---
+++ app/src/main/java/com/rathana/layout_demo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.rathana.layout_demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nested_scroll_view_layout);
+    }
+}
